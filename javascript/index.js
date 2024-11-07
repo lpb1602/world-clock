07208139f3283400f5cbaefe1de0040bf24e8013
lpb1602 <lponsb98@gmail.com>
2024-11-07 12:44:34 +0100

    Added JavaScript to display current time and date

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+function updateTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  // Paris
+  let ParisElement = document.querySelector("#paris");
+  let ParisDateElement = ParisElement.querySelector(".date");
+  let ParisTimeElement = ParisElement.querySelector(".time");
+  let ParisTime = moment().tz("Europe/Paris");
+  ParisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
+  ParisTimeElement.innerHTML = ParisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
